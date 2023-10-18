ded7f9198d278d7cc3efaf6895c0bbd26dfdeb5a
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-18 06:55:52 -0700

    set up NFT struct and counting functionality

--- contract/contracts/CeloNFTsol.sol
+++ contract/contracts/CeloNFTsol.sol
@@ -5,4 +5,29 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract CeloNFT is ERC721URIStorage {
 
+     /// @notice implement the Counter libarary for counting tokenId
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenId; // tokenId
+    Counters.Counter  private _soldItems; // total items
+    address payable owner;
+    mapping (uint256 => NFT) NFT_ID;
+
+    // nft struct
+     struct NFT {
+        address owner;
+        address seller;
+        uint256 price;
+        bool sold;
+        uint256 tokenId;
+    }
+
+     constructor() ERC721("CeloNFT", "ASG"){
+        owner = payable(msg.sender);
+    }
+    
+
+
+
+}
