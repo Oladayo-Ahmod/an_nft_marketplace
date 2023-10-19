4feaf54f66502794cf7684f699d7088d97dc230f
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-19 08:53:29 -0700

    render current user nfts

---
+++ frontend/src/app/my-nfts/page.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/app/components/Navbar";
+import Mynft from '@/app/components/Mynft'
+
+export default function MyNFT() {
+  return (
+    <main>
+      <Navbar />
+      <Mynft />
+    </main>
+  )
+}
