a3fe8177c7158dd1059d8f068892d9b49718dca7
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-18 23:14:24 -0700

    test mint and sales of nft

--- contract/test/celoNFT.test.js
+++ contract/test/celoNFT.test.js
@@ -8,19 +8,49 @@ describe('CeloNFT', ()=>{
         contract = await provider.deploy()
     })
     
-      // correct constructor
-    it("has correct constructor", async ()=>{
-        const name = await contract.name()
-        assert.equal(name,"CeloNFT")
+    describe('contract deployment', ()=>{
+        // correct constructor
+        it("has correct constructor", async ()=>{
+            const name = await contract.name()
+            assert.equal(name,"CeloNFT")
+        })
+
+        // tests for successfully deployment
+        it("deploys contract successfully", async ()=>{
+            const address = await contract.deployed()
+            assert.notEqual(address,0x0)
+            assert.notEqual(address,'')
+            assert.notEqual(address,undefined)
+            assert.notEqual(address,null)
+        })
     })
 
-     // tests for successfully deployment
-     it("deploys contract successfully", async ()=>{
-        const address = await contract.deployed()
-        assert.notEqual(address,0x0)
-        assert.notEqual(address,'')
-        assert.notEqual(address,undefined)
-        assert.notEqual(address,null)
+    // NFT functionalities
+    describe('NFT functionality', ()=>{
+         // creates token and returned ID
+         it("mint token", async ()=>{
+            const price = ethers.utils.parseEther('2')
+            const tx = await contract.createToken("https://test-url",price)
+            const receipt = await tx.wait()
+            const events = receipt.events.find(event => event.event === 'NFT_Action');
+            assert.equal(events.args[0],1) // token id equals
+        })
+
     })
+
+     // NFT sales
+     it("it sells nft",async ()=>{
+        const price = ethers.utils.parseEther('2')
+        await contract.createToken("https://test-url",price)
+        const listedPrice = await contract.getNftPrice('1') // retrieve nft price
+        const tx = await contract.sellNFT(1,{value :listedPrice})
+        const receipt = await tx.wait()
+        const events = receipt.events.find(event => event.event === 'NFT_Action');
+        assert.equal(events.args[0],1) // token id equals
+        assert.equal(events.args[4],true) // sold equals true
+    })
+
+
+
 
 })
