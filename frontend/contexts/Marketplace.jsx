58fd8a6e91453101ebfa06ef07351a74e1fd86e1
oladayo ahmod <oladayoahmod112@gmail.com>
2023-10-19 05:55:25 -0700

    wallet connection

--- frontend/contexts/Marketplace.jsx
+++ frontend/contexts/Marketplace.jsx
@@ -11,3 +11,29 @@ if(typeof window !== 'undefined'){
     connection = window.ethereum
 }
 
+// NFT marketplace contract provider provider
+const MarketplaceProvider =({children})=>{
+
+     // state variables
+     const [account , setAccount] = useState()
+     const [nftData,setNftData] = useState()
+     const [singleNftData,setSingleNftData] = useState()
+     const [nftUrl, setNftUrl] = useState()
+     const [buttonDisability, setbuttonDisability] = useState(false)
+     const [message,setMessage] = useState('List Item')
+     const [userNftData, setUserNftData] = useState('')
+     const [formData,setFormData] = useState({name: '',price : '',file :''})
+
+      // integrate wallet connection
+    const connectWallet = async function(){
+        if (connection) {
+            const accounts = await connection.request({method : 'eth_requestAccounts'})
+            setAccount(accounts[0])
+        }
+        else{
+            console.log('please install metamask')
+        }
+    }
+
+ 
+}
