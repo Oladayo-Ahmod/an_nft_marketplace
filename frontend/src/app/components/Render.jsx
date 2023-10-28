fa34d15b04dba7db2208eba4ef7d15f017a13652
Walter Brian <133803737+WalterBrian@users.noreply.github.com>
2023-10-28 15:45:51 -0400

    commit completed.

--- frontend/src/app/components/Render.jsx
+++ frontend/src/app/components/Render.jsx
@@ -1,90 +1,61 @@
-"use client"
-import { useContext, useEffect, useRef, useState } from 'react'
-import { MarketplaceContext } from "../../../contexts/Marketplace";
+import { useContext, useEffect } from 'react';
+import { MarketplaceContext } from '../../../contexts/Marketplace';
 import Image from 'next/image';
 
-const Render =()=>{
-   const { nftData,account,allListedNfts,SellNft} = useContext(MarketplaceContext)
+const Render = () => {
+    const { nftData, account, allListedNfts, SellNft } = useContext(MarketplaceContext);
 
-   useEffect(()=>{
-    allListedNfts()
-    // console.log(nftData)
-   },[allListedNfts])
+    useEffect(() => {
+        allListedNfts();
+    }, [allListedNfts]);
+
+    // Memoize the nftData if it doesn't change frequently
+    const memoizedNftData = useMemo(() => nftData, [nftData]);
+
     return (
-        <>
-         <style jsx>
-        {
-          `
-            .col-container{
-              width :280px;
-              height : 280px;
-              text-align : center;
-              margin :20px 20px;
-              padding : 20px 5px 0 5px;
-            }
-            .caption{
-                margin-left: 20px;
-            }
-            .caption-tp{
-                position :relative;
-                bottom : -35px;
-                left : 15%;
-            }
-            .caption-bm{
-                position: relative;
-                bottom: 60px;
-                left: 40px;
-            }
-            .btn{
-                position: relative;
-                margin-top: -40%;
-                left: 25%;
-            }
-
-          `
-        }
-        </style>
-
-             <div className='container mt-5'>
-                <div className='row justify-content-center align-items-center '>
-                    {
-                        nftData? nftData.map((nft,i)=>(
-                            <div className='col col-md-3' key={nft.id}>
-                            <small className="text-info fw-bold caption caption-tp ">{nft.name} </small>
-                            <div className=" col-container rounded">
-                                <Image className='image'  loader={()=>nft.image} src={nft.image} width={200} height={200} alt={nft.name} />
+        <div className="container mt-5">
+            <div className="row justify-content-center align-items-center">
+                {memoizedNftData ? (
+                    memoizedNftData.map((nft, i) => (
+                        <div className="col col-md-3" key={nft.id}>
+                            <small className="text-info fw-bold caption caption-tp">{nft.name}</small>
+                            <div className="col-container rounded">
+                                <Image
+                                    className="image"
+                                    loader={() => nft.image}
+                                    src={nft.image}
+                                    width={200}
+                                    height={200}
+                                    alt={nft.name}
+                                />
                             </div>
-                            <small className="text-secondary fw-bold caption-bm caption">Price : {nft.price}ETH </small>
-                            <p className="text-secondary fw-bold caption-bm caption">Seller: {nft.seller ? `${nft.owner.slice(0,6)}...${nft.owner.slice(nft.owner.length -4)}` : ''} </p>
-                            {
-                                Number(account) == Number(nft.seller)  || account == Number(nft.owner) ?
-                                <button className='btn btn-success col-md-8 ml-3' disabled>
-                                owned by you
+                            <small className="text-secondary fw-bold caption-bm caption">
+                                Price: {nft.price} ETH
+                            </small>
+                            <p className="text-secondary fw-bold caption-bm caption">
+                                Seller: {nft.seller ? `${nft.owner.slice(0, 6)}...${nft.owner.slice(-4)}` : ''}
+                            </p>
+                            {Number(account) === Number(nft.seller) || account === Number(nft.owner) ? (
+                                <button className="btn btn-success col-md-8 ml-3" disabled>
+                                    Owned by you
                                 </button>
-                                :
-                                (nft.sold ?
-                                    <button className='btn btn-danger col-md-8 ml-3' disabled>
-                                    sold
-                                    </button>
-                                 : <button className='btn btn-primary col-md-8 ml-3' onClick={()=>SellNft(nft.tokenId)}>
+                            ) : nft.sold ? (
+                                <button className="btn btn-danger col-md-8 ml-3" disabled>
+                                    Sold
+                                </button>
+                            ) : (
+                                <button className="btn btn-primary col-md-8 ml-3" onClick={() => SellNft(nft.tokenId)}>
                                     Buy
-                                </button>)
-
-
-                            }
-
-
+                                </button>
+                            )}
                         </div>
-                        ))
-                        :
-                        <h1 className='text-info fw-bold'>Loading NFT items ...</h1>
-                    }
+                    ))
+                ) : (
+                    <h1 className="text-info fw-bold">Loading NFT items...</h1>
+                )}
+            </div>
+        </div>
+    );
+};
 
-                </div>
-          </div>
-
-        </>
-    )
-}
-
-export default Render
+export default Render;
