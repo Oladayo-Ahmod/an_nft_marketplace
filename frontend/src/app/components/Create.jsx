f12719fb7035391f313f9224249b173c75b58d57
Oladayo Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2023-11-02 19:17:53 -0700

    Revert "updated fix"

--- frontend/src/app/components/Create.jsx
+++ frontend/src/app/components/Create.jsx
@@ -1,85 +1,54 @@
-import React, { useContext } from 'react';
-import { MarketplaceContext } from '../../../contexts/Marketplace';
+"use client"
 
-/**
- * Create component for adding new NFT items.
- */
-const Create = () => {
-  // Accessing context and required functions
-  const { disability, setFormData, formData, CreateNft, message, imageHandler } = useContext(MarketplaceContext);
+import { useContext, useEffect, useRef, useState } from 'react'
+import { MarketplaceContext } from "../../../contexts/Marketplace";
 
-  return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-lg-7">
-          <div className="form-wrapper">
-            <form className="row" action="#">
-              {/* Item Name */}
-              <div className="col-md-12 m-2">
-                <div className="input-box">
-                  <label htmlFor="name" className="form-label">
-                    Item Name
-                  </label>
-                  <input
-                    id="name"
-                    className="form-control"
-                    placeholder="e.g. 'Cute Dog'"
-                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                  />
+ const Create = ()=>{
+    const {disability,setFormData,formData,CreateNft,message,imageHandler} = useContext(MarketplaceContext)
+
+    return(
+        <>
+            <div className='container mt-5'>
+               <div className='row justify-content-center'>
+               <div className="col-lg-7">
+                        <div className="form-wrapper">
+                            <form className="row" action="#">
+                                <div className="col-md-12 m-2">
+                                    <div className="input-box">
+                                        <label for="name" className="form-label">Item Name</label>
+                                        <input id="name" className='form-control' placeholder="e. g. `Cute Dog`" onChange={e => setFormData({...formData, name : e.target.value  })} />
+                                    </div>
+                                </div>
+
+
+                                <div className="col-md-12 mt-2">
+                                    <div className="input-box">
+                                        <label for="dollerValue" className="form-label">Item Image</label> <br />
+                                        <input name="createinputfile"  type="file" className="inputfile" onChange={e=>imageHandler(e)} required/>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-12 mt-2">
+                                    <div className="input-box">
+                                        <label for="dollerValue" className="form-label">Item Price</label>
+                                        <input id="dollerValue" className='form-control' required placeholder="e. g. `20ETH`" onChange={e => setFormData({...formData, price : e.target.value  })} />
+                                    </div>
+                                </div>
+
+                                <div className="col-md-12 mt-2 col-xl-8 mt_lg--15 mt_md--15 mt_sm--15">
+                                    <div className="input-box">
+                                        <button className="btn btn-primary btn-large w-100" disabled={disability} type='button' onClick={CreateNft} >{message}</button>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+
                 </div>
-              </div>
+               </div>
+            </div>
+        </>
+    )
+}
 
-              {/* Item Image */}
-              <div className="col-md-12 mt-2">
-                <div className="input-box">
-                  <label htmlFor="createinputfile" className="form-label">
-                    Item Image
-                  </label>
-                  <input
-                    name="createinputfile"
-                    type="file"
-                    className="inputfile"
-                    onChange={(e) => imageHandler(e)}
-                    required
-                  />
-                </div>
-              </div>
-
-              {/* Item Price */}
-              <div className="col-md-12 mt-2">
-                <div className="input-box">
-                  <label htmlFor="dollerValue" className="form-label">
-                    Item Price
-                  </label>
-                  <input
-                    id="dollerValue"
-                    className="form-control"
-                    required
-                    placeholder="e.g. '20ETH'"
-                    onChange={(e) => setFormData({ ...formData, price: e.target.value })}
-                  />
-                </div>
-              </div>
-
-              {/* Submit Button */}
-              <div className="col-md-12 mt-2 col-xl-8 mt_lg--15 mt_md--15 mt_sm--15">
-                <div className="input-box">
-                  <button
-                    className="btn btn-primary btn-large w-100"
-                    disabled={disability}
-                    type="button"
-                    onClick={CreateNft}
-                  >
-                    {message}
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Create;
+export default Create
--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -1,58 +1,49 @@
-import React, { useContext } from 'react';
-import { MarketplaceContext } from '../../../contexts/Marketplace';
+"use client"
+
+import { useContext, useEffect, useRef, useState } from 'react'
+import { MarketplaceContext } from "../../../contexts/Marketplace";
 import Link from 'next/link';
 
-const Navbar = () => {
-    const { account, connectWallet } = useContext(MarketplaceContext);
+const Navbar =()=>{
 
-    const renderAccountButton = () => {
-        if (account) {
-            const shortAccount = `${account.slice(0, 6)}...${account.slice(account.length - 4)}`;
-            return (
-                <button className="btn btn-outline-success my-2 my-sm-0" type="button">
-                    {shortAccount}
-                </button>
-            );
-        } else {
-            return (
-                <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={connectWallet}>
-                    Connect Wallet
-                </button>
-            );
-        }
-    };
+    const {account,connectWallet} = useContext(MarketplaceContext)
+
+    // bootstrap
+    useEffect(()=>{
+        require('bootstrap/dist/js/bootstrap.bundle')
+    },[])
+
+    // wallet connection
+    useEffect(()=>{
+        connectWallet()
+      },[])
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow-sm">
-            <Link href={'/'} passHref>
-                <a className="navbar-brand text-light">NFT Marketplace</a>
-            </Link>
-            <button
-                className="navbar-toggler d-lg-none"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapsibleNavId"
-                aria-controls="collapsibleNavId"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            />
+
+            <nav className="navbar navbar-expand-sm navbar-dark bg-white shadow-sm">
+            <Link className='ml-1 navbar-brand text-secondary' href={'/'}> NFT Marketplace </Link>
+            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation"></button>
             <div className="collapse navbar-collapse" id="collapsibleNavId">
                 <ul className="navbar-nav me-auto mt-2 mt-lg-0 text-secondary">
                     <li className="nav-item">
-                        <Link href="/create-nft" passHref>
-                            <a className="nav-link text-info">Create NFT</a>
-                        </Link>
+                    <Link className="nav-link  text-info" href={'/create-nft'}> Create NFT </Link>
                     </li>
                     <li className="nav-item">
-                        <Link href="/my-nfts" passHref>
-                            <a className="nav-link text-info">My NFTs</a>
-                        </Link>
+                    <Link className="nav-link  text-info" href={'/my-nfts'}> My NFT </Link>
                     </li>
                 </ul>
-                <form className="d-flex my-2 my-lg-0 px-2">{renderAccountButton()}</form>
+                <form className="d-flex my-2 my-lg-0 px-2">
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={connectWallet} >  {account ? `${account.slice(0,6)}...${account.slice(account.length -4)}` : 'connect wallet'}
+                    </button>
+                </form>
             </div>
-        </nav>
-    );
-};
+            </nav>
 
-export default Navbar;
+
+    )
+
+
+}
+
+export default Navbar
