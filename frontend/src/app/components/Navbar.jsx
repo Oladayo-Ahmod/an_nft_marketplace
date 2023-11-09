b2576f4e0808fb63ebbdd7ffac70255e6de5aba5
oladayo ahmod <oladayoahmod112@gmail.com>
2023-11-09 05:59:35 -0800

    fix navbar toggler

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -22,8 +22,15 @@ const Navbar =()=>{
 
             <nav className="navbar navbar-expand-sm navbar-dark bg-white shadow-sm">
             <Link className='ml-1 navbar-brand text-secondary' href={'/'}> NFT Marketplace </Link>
-            <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                aria-expanded="false" aria-label="Toggle navigation"></button>
+              <div className="container">
+                <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+              
+                </div>
+        
+
             <div className="collapse navbar-collapse" id="collapsibleNavId">
                 <ul className="navbar-nav me-auto mt-2 mt-lg-0 text-secondary">
                     <li className="nav-item">
